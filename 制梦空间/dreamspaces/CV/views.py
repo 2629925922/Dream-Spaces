1d28afbb09c140309d2459e3360dc532a0fde512
2629925922 <73739868+2629925922@users.noreply.github.com>
2021-11-09 12:13:47 +0800

    Add files via upload

--- 制梦空间/dreamspaces/CV/views.py
+++ 制梦空间/dreamspaces/CV/views.py
@@ -1,5 +1,31 @@
 from django.shortcuts import render
+import csv
+import os
+from django.core.paginator import PageNotAnInteger,EmptyPage,Paginator
 
 # Create your views here.
-def index(request):
-    return render(request,'CV.html')
+
+def load_file(name):
+    data = []
+    # dir = os.path.abspath(os.path.join(os.path.dirname('settings.py'),os.path.pardir))
+    # dir = dir.replace("\\",'/')
+    # aa = r"/static/images/TP/"+ name
+    # files = os.path.join(dir,aa)
+    files = r'C:\Users\Think\Desktop\文件\制梦空间 django项目\制梦空间\dreamspaces\static\images\TP\\'+name
+    for file in os.listdir(files):
+        data.append('/static/images/TP/'+ name +'/' + file)
+    return data
+
+def index(request,name='fj',page=1):
+    data =  load_file(name)
+    lists = Paginator(data,10)
+    try:
+        pages = lists.page(page)
+    except PageNotAnInteger:
+        pages = lists.page(1)
+    except EmptyPage:
+        pages = lists.page(lists.num_pages)
+    return render(request,'bz.html',{"data":pages,"page_ranges":lists})
+
+
+
