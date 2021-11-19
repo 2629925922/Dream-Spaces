0535d2cc57e8cb48d0ad1f4563f6ba1a96be762f
2629925922 <73739868+2629925922@users.noreply.github.com>
2021-11-19 11:43:10 +0800

    Add files via upload

--- 制梦空间/dreamspaces/bz/views.py
+++ 制梦空间/dreamspaces/bz/views.py
@@ -1,20 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 import csv
 import os
 from django.core.paginator import PageNotAnInteger,EmptyPage,Paginator
+from .models import Tk
 
 # Create your views here.
+def urls(name):
+    url = r'C:\Users\Think\Desktop\文件\制梦空间 django项目\制梦空间\dreamspaces\static\images\TP\\' + name
+    return url
 
 def load_file(name):
     data = []
-    files = r'C:\Users\Think\Desktop\文件\制梦空间 django项目\制梦空间\dreamspaces\static\images\TP\\'+name
-    for file in os.listdir(files):
-        data.append('../static/images/TP/'+ name +'/' + file)
+    # dir = os.path.abspath(os.path.join(os.path.dirname('settings.py'),os.path.pardir))
+    # dir = dir.replace("\\",'/')
+    # aa = r"/static/images/TP/"+ name
+    # files = os.path.join(dir,aa)
+    for file in os.listdir(urls(name)):
+        data.append('/static/images/TP/'+ name +'/' + file)
     return data
 
 def index(request,name='fj',page=1):
-    data =  load_file(name)
-    lists = Paginator(data,10)
+    data = Tk.objects.filter(url__icontains=name)
+    # data =  load_file(name)
+    lists = Paginator(data,12)
     try:
         pages = lists.page(page)
     except PageNotAnInteger:
@@ -23,5 +31,16 @@ def index(request,name='fj',page=1):
         pages = lists.page(lists.num_pages)
     return render(request,'bz.html',{"data":pages,"page_ranges":lists})
 
-
+def search(request):
+    # names = ['bj','dm','fj','mn','mx','yx']
+    # for name in names:
+    #     for file in os.listdir(urls(name)):
+    #         url = '/static/images/TP/'+ str(name) +'/' + str(file)
+    #         data = Tk.objects.create(name=file,url=url)
+    name = request.GET.get("tp_name")
+    data = Tk.objects.filter(name__icontains=name)
+    count = data.count()
+    for i in data:
+        print(i.url)
+    return render(request,'bz_search.html',{'data':data,"count":count})
 
